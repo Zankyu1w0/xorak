80397126ca1d2e1c241a7c7148498757dc08cd8e
Zankyu1w0 <supertinom1@gmail.com>
2026-03-28 15:41:14 +0300

    Update domain range in check_domain function

--- mojari.py
+++ mojari.py
@@ -37,7 +37,7 @@ CHANNELS = [
 ]
 
 def check_domain(index):
-    """10-99 arası domainleri hızlıca test eder."""
+    """12-99 arası domainleri hızlıca test eder."""
     url = BASE_PATTERN.format(index)
     try:
         response = requests.get(url, headers=HEADERS, timeout=5, verify=False)
@@ -52,12 +52,12 @@ def main():
     if not os.path.exists(OUTPUT_FOLDER):
         os.makedirs(OUTPUT_FOLDER)
 
-    print("🔍 Andro Panel aktif domaini aranıyor (10-99 arası)...")
+    print("🔍 Andro Panel aktif domaini aranıyor (12-99 arası)...")
     
     # 1. Aktif Domaini Bul
     active_site = None
     with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-        futures = [executor.submit(check_domain, i) for i in range(10, 100)]
+        futures = [executor.submit(check_domain, i) for i in range(12, 100)]
         for future in concurrent.futures.as_completed(futures):
             result = future.result()
             if result:
@@ -66,7 +66,7 @@ def main():
                 break
 
     if not active_site:
-        print("❌ 10 ile 99 arasında aktif site bulunamadı.")
+        print("❌ 12 ile 99 arasında aktif site bulunamadı.")
         return
 
     print(f"✅ Güncel domain bulundu: {active_site}")
